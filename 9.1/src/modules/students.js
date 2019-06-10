8ce177d797d92ae8ac1b082841600e351b940a19
OlgaShtante <olga.shtante@gmail.com>
2019-06-10 23:25:24 +0300

    correct text message in the students.js

--- 9.1/src/modules/students.js
+++ 9.1/src/modules/students.js
@@ -1,28 +1,26 @@
-function Students(boys, girls){
+function Students(boys, girls) {
   this.boys = boys;
   this.girls = girls;
   this.group = "M-FD2-54-19";
   this.boys = studentParam;
 
+  const askQuestion = function(studentParam) {
+    alert(`students ${studentParam} ask questions`);
+  };
+  askQuestion("often");
 
-const askQuestion = function(studentParam){
-  alert(`students ${studentParam} ask questions`);
-} 
-askQuestion('often');
-
-  this.doHomework = function(){
-  this.group = "evening group";
-  console.log(`${this.group} starts clases at 18:30 on Wednesday`)
-  }
+  this.doHomework = function() {
+    this.group = "evening group";
+    console.log(`${this.group} starts classes at 18:30 on Wednesday`);
+  };
 }
 
-Students.prototype.study = function(){
+Students.prototype.study = function() {
   this.boys = 10;
-  this.girls = 3; 
+  this.girls = 3;
   const studyProcess = "students are studying!";
   this.group = this.boys + this.girls;
   console.log(`${this.group} ${studyProcess}`);
-}
+};
 
 export { Students };
-
