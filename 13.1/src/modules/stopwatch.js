0c2c790f9c4c9d26ce2eb3bfa0151f6e587a7c7d
OlgaShtante <olga.shtante@gmail.com>
2019-06-19 17:40:10 +0300

    initial commit 13.1 and update index.html

---
+++ 13.1/src/modules/stopwatch.js
@@ -0,0 +1,11 @@
+import { StopwatchTimer } from "./stopwatchTimer.js";
+function Stopwatch() {
+  StopwatchTimer.call(this, "stopwatch", 0);
+}
+
+Stopwatch.prototype = Object.create(StopwatchTimer.prototype);
+Stopwatch.prototype.constructor = Stopwatch;
+Stopwatch.prototype.showInfo = function() {
+  console.log(this);
+};
+export { Stopwatch };
