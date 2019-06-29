b0dc53dc11dfafc49b70a8905e8441a0ae9ab121
OlgaShtante <olga.shtante@gmail.com>
2019-06-30 00:20:40 +0300

    update 1.1 with folder structure

---
+++ 1.1/src/modules/main.js
@@ -0,0 +1,13 @@
+function plus() {
+  let num1, num2, sum;
+
+  num1 = document.getElementById("inp1").value;
+  num1 = parseInt(num1);
+
+  num2 = document.getElementById("inp2").value;
+  num2 = parseInt(num2);
+
+  sum = num1 + num2;
+  debugger;
+  alert(sum);
+}
