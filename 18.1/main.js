cb735faf4cb93e3aaaa3bcc682792eac3bb1c3aa
OlgaShtante <olga.shtante@gmail.com>
2019-07-02 17:59:09 +0300

    initial commit 18.1 and update  index.html

---
+++ 18.1/main.js
@@ -0,0 +1,18 @@
+let cardSlideIndex = 0;
+
+function showSlides() {
+  const cardSlide = document.getElementsByClassName("slide");
+
+  for (i = 0; i < cardSlide.length; i++) {
+    cardSlide[i].style.display = "none";
+  }
+  cardSlideIndex++;
+  if (cardSlideIndex > cardSlide.length) {
+    cardsSlideIndex = 1;
+  }
+  cardSlide[cardSlideIndex - 1].style.display = "block";
+
+  setTimeout(showSlides, 3000);
+}
+
+showSlides();
