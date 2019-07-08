96dc7df28b6057c3bcf20792000f4178484fa168
OlgaShtante <olga.shtante@gmail.com>
2019-07-09 01:18:29 +0300

    initial commit of 19.1,update all tasks html doc

---
+++ 19.1/src/modules/main.js
@@ -0,0 +1,29 @@
+function delay(milliseconds) {
+  let myPromise = new Promise(function(resolve) {
+    setTimeout(resolve, milliseconds);
+  });
+  return myPromise;
+}
+
+function textMessage() {
+  console.log("Delay messages:");
+}
+
+delay(1000)
+  .then(textMessage)
+  .then(() => {
+    delay(3000)
+      .then(function() {
+        console.log("The delay of this message is 3 sec.");
+      })
+      .then(function() {
+        console.log("This delay is equal to the delay of the message above!");
+      })
+      .then(() => {
+        delay(1000).then(function() {
+          console.log(
+            "This message appears in 1 sec after the message with 3 sec delay."
+          );
+        });
+      });
+  });
